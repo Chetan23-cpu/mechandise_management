d4b564a56b027ec73acd51cd3606725cb7e80a4a
Chetan23-cpu <tr.qa@tracesci.com>
2026-09-04 14:01:48 +0530

    changes

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,17 @@
 require("dotenv").config();
 
-module.exports = {
-  development: {
-    client: "mysql2",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./migrations",
-    },
-    seeds: {
-      directory: "./seeds",
-    },
+const config = {
+  client: "mysql2",
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: "./migrations",
   },
+  seeds: {
+    directory: "./seeds",
+  },
+};
+
+module.exports = {
+  development: config,
+  production: config,
 };
